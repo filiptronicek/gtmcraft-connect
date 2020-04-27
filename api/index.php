1dc5adb54142f643bc8221f81bd1c60c7cb3173e
Filip Troníček <filip.tronicek@seznam.cz>
2020-04-27 22:27:49 +0200

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,9 +1,2 @@
-<?php                                                                                                                                       
-  error_reporting(E_ERROR);                                                                                                                   
-  $fp = fsockopen('0.tcp.eu.ngrok.io:12604', 25565, $errno, $errstr, 1);                                                                                
-  if (!$fp) {                                                                                                                                 
-    echo 'no';                                                                                                                                
-  } else {                                                                                                                                    
-    echo 'yes';                                                                                                                               
-    fclose($fp);                                                                                                                              
-  }                                                                                                                                           
+<?php
+phpinfo();
