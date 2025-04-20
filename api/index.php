1ba1d9cb755af7928d8b6600d8de97a42da4f6a4
Filip Troníček <filip@gitpod.io>
2025-04-20 21:43:06 +0200

    Clean up

--- api/index.php
+++
@@ -1,8 +0,0 @@
- <?php                                                                                                                                       
-  $fp = fsockopen($_GET['a'], $_GET['p'], $errno, $errstr, 1);                                                                                
-  if (!$fp) {                                                                                                                                 
-    echo 'no';                                                                                                                                
-  } else {                                                                                                                                    
-    echo 'yes';                                                                                                                               
-    fclose($fp);                                                                                                                              
-  }         
